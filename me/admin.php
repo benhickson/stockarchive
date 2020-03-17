a4c95b87cf695136c6fba9ab1caad107f7338767
Kevin Nittolo <15949490+nittolo@users.noreply.github.com>
2020-03-16 22:30:14 -0400

    fixing author email

--- me/admin.php
+++ me/admin.php
@@ -1,2 +1,22 @@
-<?php
-echo 'admin page';
+<style>
+  select {
+    display: inline;
+    max-width: 500px;
+  }
+</style>
+
+<div class="input-field searchstuff">
+  <select id="id">
+    <option value="0" selected>Select User</option>
+    <?php
+    $cols = array("id, email, firstname, lastname");
+    $db->orderBy("firstname","asc");
+    $users = $db->get("users", null, $cols);
+    foreach ($users as $user) {
+      echo '<option value="'.$user['id'].'">'.$user['firstname'].' '.$user['lastname'].'</option>'."\n";
+    }
+    ?>
+  </select>
+  <button type="button">edit</button>
+  <button type="button">new user</button>
+</div>
