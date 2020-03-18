2c425f0c6572ba5ba09a919483dd44aa08338919
Kevin Nittolo <15949490+nittolo@users.noreply.github.com>
2020-03-17 23:14:04 -0400

    drop downs added

---
+++ ajax/users.php
@@ -0,0 +1,45 @@
+<?php
+
+require __DIR__.'/../includes/0-base.php';
+
+if ($_SESSION['logged_in']) {
+	// add the userid from session
+	$userid = $_SESSION['userid'];
+
+	// check if all necessary fields set
+	if (isset($_POST['userId'])) {
+		$id = $_POST['userId'];
+
+		$cols = array(
+			" id
+			, email
+			, firstname
+			, lastname
+			, interfaceprefs
+      , registration_open
+      , registration_code
+      , picture
+      , temp
+      , old
+      , able_unpublish
+    ");
+    $db->where('id', $id);
+    $userData = $db->get("users", null, $cols);
+    $userData['tagsuccess'] = true;
+
+    exit(json_encode($userData[0]));
+
+	} else {
+		// postfields not set correctly
+		exit(json_encode(array(
+			'tagsuccess' => false
+			, 'message' => 'Incorrect postfields set.'
+		)));
+	}
+} else {
+	// not logged in
+	exit(json_encode(array(
+		'tagsuccess' => false
+		, 'message' => 'Not logged in.'
+	)));
+}
--- me/admin.php
+++ me/admin.php
@@ -6,17 +6,56 @@
 </style>
 
 <div class="input-field searchstuff">
-  <select id="id">
+  <select id="user">
     <option value="0" selected>Select User</option>
     <?php
-    $cols = array("id, email, firstname, lastname");
-    $db->orderBy("firstname","asc");
-    $users = $db->get("users", null, $cols);
-    foreach ($users as $user) {
-      echo '<option value="'.$user['id'].'">'.$user['firstname'].' '.$user['lastname'].'</option>'."\n";
-    }
+      $cols = array("id, email, firstname, lastname");
+      $db->orderBy("firstname","asc");
+      $users = $db->get("users", null, $cols);
+      
+      foreach ($users as $user) {
+        echo '<option value="'.$user['id'].'">'.$user['firstname'].' '.$user['lastname'].'</option>'."\n";
+      }
     ?>
   </select>
-  <button type="button">edit</button>
-  <button type="button">new user</button>
+  <button type="button" onclick="editUser()">edit</button>
+  <button type="button" onclick="addUser()">new user</button>
+  <div id="userInfoPanel" style="width: 500px">
+    <form action="/action_page.php">
+      <select id="userDataSelect">
+        <option value="0" selected>Data</option>
+      </select>
+      <input type="submit" value="Submit">
+    </form> 
+  </div>
 </div>
+
+<script type="text/javascript">
+  function addUser() {
+    console.log('add');
+  }
+
+  function openUserInfoPanel(userInfo) {
+
+  }
+
+  function editUser() {
+    var e = document.getElementById("user");
+    var userId = e.options[e.selectedIndex].value;
+    console.log(userId);
+
+    $.ajax('../ajax/users.php', {
+      type: 'POST',
+      data: {userId: userId},
+      success: function(res) {
+        var userInfo = JSON.parse(res);
+
+        openUserInfoPanel(userInfo);
+      },
+      error: function(xhr, status, error) {
+        var err = JSON.parse(xhr.responseText);
+        console.log(err.Message);
+      }
+    });
+  }
+</script>
