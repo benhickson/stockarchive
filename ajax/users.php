c89f30b179dc57f28467a35f23b46d219a585b7d
Kevin Nittolo <15949490+nittolo@users.noreply.github.com>
2020-03-24 18:14:17 -0400

    can send ajax request to change field value

--- ajax/users.php
+++ ajax/users.php
@@ -2,20 +2,21 @@
 
 require __DIR__.'/../includes/0-base.php';
 
+// @@TODO: kick off those that aren't able_unpublish
+
 if($_SESSION['logged_in']) {
-	// add the userid from session
-	$userid = $_SESSION['userid'];
+  // add the userid from session
+  $userid = $_SESSION['userid'];
 
-	// check if all necessary fields set
-	if(isset($_POST['userId'])) {
-		$id = $_POST['userId'];
+  // check if all necessary fields set
+  if(isset($_POST['userId'])) {
+    $id = $_POST['userId'];
 
-		$cols = array(
-			" id
-			, email
-			, firstname
-			, lastname
-			, interfaceprefs
+    $cols = array(
+      " id
+      , email
+      , firstname
+      , lastname
       , registration_open
       , registration_code
       , picture
@@ -29,24 +30,49 @@ if($_SESSION['logged_in']) {
 
     exit(json_encode($userData[0]));
 
-  } else if(isset($_POST['get_columns'])) {
+  } 
+  else if(isset($_POST['get_columns'])) {
     $sql = 'SHOW COLUMNS FROM users';
     $res = $db->query($sql);
 
     exit(json_encode(array(
-			$res
+      $res
     )));
-	} else {
-		// postfields not set correctly
-		exit(json_encode(array(
-			'tagsuccess' => false
-			, 'message' => 'Incorrect postfields set.'
-		)));
+  } 
+  else if(isset($_POST['user_id'])
+  && isset($_POST['value'])
+  && isset($_POST['field'])
+  ) { // exit(json_encode($_POST)); //@@
+    $id = $_POST['user_id'];
+
+    $db->where('id', $id);
+    $res = $db->update($_POST['field'], $_POST['value']);
+
+    if($res) {
+      echo json_encode(array(
+        'tagsuccess' => true
+        , 'message' => 'User "'.$edit_field.'" editted to be ``'.edit_info.'``'
+      ));
+    }
+    else {
+      echo json_encode(array(
+        'tagsuccess' => false
+        , 'message' => 'Failed to edit user "'.$edit_field.'" editted to be ``'.edit_info.'``.'
+      ));
+    }
+  } 
+  else {
+    // postfields not set correctly
+    exit(json_encode(array(
+      'tagsuccess' => false
+      , 'message' => 'Incorrect postfields set.'
+    )));
   }  
-} else {
-	// not logged in
-	exit(json_encode(array(
-		'tagsuccess' => false
-		, 'message' => 'Not logged in.'
-	)));
+} 
+else {
+  // not logged in
+  exit(json_encode(array(
+    'tagsuccess' => false
+    , 'message' => 'Not logged in.'
+  )));
 }
--- me/admin.php
+++ me/admin.php
@@ -21,26 +21,45 @@
   <button type="button" onclick="editUser()">edit</button>
   <button type="button" onclick="addUser()">new user</button>
   <div id="userInfoPanel" style="width: 500px">
-    <form action="/action_page.php">
-      <select id="userDataSelect">
-        <option value="0" selected disabled>Data</option>
-      </select>
-      <form>
-        <label for="editField">Edit:</label><br>
-        <input type="text" id="editField" name="editField"><br>
-      </form>
-      <input type="submit" value="Submit">
-      <div>int(11) [Integer], varchar(255) [Alphanumberic Characters], tinyint(1) [1 for true, 0 for false], char(2) [2 Letters]</div>
-    </form> 
+    <select id="userDataSelect">
+      <option value="0" selected disabled>Data</option>
+    </select>
+    <form>
+      <label for="editField">Edit:</label><br>
+      <input type="text" id="editField" name="editField"><br>
+    </form>
+    <input type="submit" value="Submit" onclick="submitUserEdit()">
   </div>
 </div>
 
 <script type="text/javascript">
   function addUser() {
-    console.log('add');
+    console.log('@@add');
   }
 
-  function openUserInfoPanel(userInfo, cols) { console.log('@@ouip', userInfo);
+  function submitUserEdit() {
+    var user = document.getElementById("user");
+    var select = document.getElementById("userDataSelect");
+    var newFieldValue = document.getElementById("editField").value;
+
+    var i = select.selectedIndex;
+    var field = select.options[i].value;
+    var userId = user.options[i].value; console.log("@@", userId, field, newFieldValue);
+    
+    $.ajax('../ajax/users.php', {
+      type: 'POST',
+      data: {user_id: userId, field: field, value: newFieldValue},
+      success: function(res) { 
+        console.log('@@edit user', res);
+      },
+      error: function(xhr, status, error) {
+        var err = JSON.parse(xhr.responseText);
+        console.log(err.Message);
+      }
+    });
+  }
+
+  function openUserInfoPanel(userInfo, cols) { console.log('@@ouip', cols);
     var select = document.getElementById("userDataSelect");
 
     cols.forEach(function(col) {
@@ -56,8 +75,42 @@
       console.log('@@ui', userInfo[select.options[i].value]);
 
       var e = document.getElementById("editField");
+      var value = select.options[i].value;
+      var curr = userInfo[value];
+      var placeholder = "";
 
-      e.value = userInfo[select.options[i].value];
+      if(curr === null || typeof curr === 'undefined') {
+        curr = "";
+        if(value === "password") {
+          placeholder = "This field is protected. ";
+        }
+        else {
+          placeholder = "This field is unset. ";
+        }
+      }
+
+      var col = cols.find(function(elem) { 
+        return elem['Field'] == value;
+      }); console.log("@@col ", col);
+
+      var type = col["Type"];
+      var typeDetails = "";
+
+      if(type === "int(11) unsigned") {
+        typeDetails = "Enter an integer.";
+      }
+      else if(type === "varchar(255)") {
+        typeDetails = "Enter various characters.";
+      }
+      else if(type === "tinyint(1)") {
+        typeDetails = "Enter 1 for true, 0 for false.";
+      }
+      else if(type === "char(2)") {
+        typeDetails = "Enter 2 letters.";
+      }
+
+      e.value = curr;
+      e.placeholder = placeholder + typeDetails;
     };
   }
 
